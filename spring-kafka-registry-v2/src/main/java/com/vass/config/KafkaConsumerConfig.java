eb04f771da971f1e66576d338bff05417f594989
François Poirier <fpoirier2002@gmail.com>
2022-03-23 17:52:14 +0100

    full branch

---
+++ spring-kafka-registry-v2/src/main/java/com/vass/config/KafkaConsumerConfig.java
@@ -0,0 +1,47 @@
+package com.vass.config;
+
+import com.vass.model.Customer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Value("${spring.kafka.schema.registry.url}")
+    private String schemaRegistry;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Long, Customer> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, Customer> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(kafkaProperties.getListener().getConcurrency());
+        return factory;
+    }
+
+    @Bean
+    ConsumerFactory<Long, Customer> consumerFactory() {
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
